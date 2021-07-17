2d7b8fdad643b16d6b2e631b13dc43c278a5d70f
PanosRCng <panosrcng@gmail.com>
2021-07-18 02:12:49 +0300

    init commit

--- NDS/Feeder.py
+++ NDS/Feeder.py
@@ -50,7 +50,7 @@ class Feeder:
 
             article = msg.value
 
-            tokens = tokenizer.tokenize(article['title'], tokens_min_length=3) + tokenizer.tokenize(article['content'], tokens_min_length=3)
+            tokens = tokenizer.tokenize(article['title']) + tokenizer.tokenize(article['content'])
 
             if len(tokens) < stopwords_config['text_min_tokens_number']:
                 continue
